80bee4172348dc925aae2f59e69b69e50e9970f1
Ventengo1 <akshayshah0628@gmail.com>
2025-02-24 17:24:22 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,34 @@
 from IPython import get_ipython
 from IPython.display import display
 # %%
+from IPython import get_ipython
+from IPython.display import display
+# %%
+
 
 # %%
+
 
 from IPython import get_ipython
 import numpy as np
 import pandas as pd
 import math
-import yfinance as yf
+import yfinance as yf  # Added yfinance
+
 
 from matplotlib import rc
 from pandas.plotting import register_matplotlib_converters
 import matplotlib.pyplot as plt
 from pylab import rcParams
-from pandas_datareader import data
+# from pandas_datareader import data  # Commented out data from pandas_datareader
 from scipy import signal
 
 
 import os
 import datetime as dt
 import seaborn as sns
+
+
 
 
 # %%
@@ -31,283 +39,445 @@ register_matplotlib_converters()
 sns.set(style='whitegrid', palette='muted', font_scale=1.5)
 rcParams['figure.figsize'] = 22, 10
 
+
 # %%
 # COMMODITY
 dateTimeObj = dt.datetime.now()
 today = dateTimeObj.strftime("%Y-%m-%d")
 
+
 symbol = "AAPL"
 date = today
 # filename = '/data/%s/Yahoo_BTCUSD_d.csv.ta.csv' % symbol
 
+
 # %%
+
 
 # **************************************************************************
 # download from yahoo the daily charts
 # **************************************************************************
 
 
+
+
 def download(symbol, date, days=365):
-               if date is None:    
-                   dateTimeObj = dt.datetime.now()
-               else:
-                   dateTimeObj = dt.datetime.strptime(date, "%Y-%m-%d")
+    if date is None:
+        dateTimeObj = dt.datetime.now()
+    else:
+        dateTimeObj = dt.datetime.strptime(date, "%Y-%m-%d")
 
-               date = dateTimeObj.strftime("%Y-%m-%d")
-               date_start = (dateTimeObj - dt.timedelta(days=days)).strftime("%Y-%m-%d")
 
-               df_source = yf.download(symbol, start=date_start, end=date)  # Use yfinance
+    date = dateTimeObj.strftime("%Y-%m-%d")
+    date_start = (dateTimeObj - dt.timedelta(days=days)).strftime("%Y-%m-%d")
 
-               #Rename columns
-               df_source = df_source.rename(columns={"Open": "open", "High": "high", "Low": "low", "Close": "Close", "Adj Close": "adj_close", "Volume": "volume"})
 
-               df_source['Date'] = pd.to_datetime(df_source.index)
-               df_source = df_source.reset_index(drop=True)
-               if 'adj_close' in df_source.columns: # Change to 'adj_close'
-                   df_source = df_source.drop(columns=['adj_close'])
-               
-               return df_source
+    df_source = yf.download(symbol, start=date_start, end=date)  # Use yfinance
+
+
+    # Rename columns
+    df_source = df_source.rename(columns={
+        "Open": "open",
+        "High": "high",
+        "Low": "low",
+        "Close": "Close",
+        "Adj Close": "adj_close",
+        "Volume": "volume"
+    })
+
+
+    df_source['Date'] = pd.to_datetime(df_source.index)
+    df_source = df_source.reset_index(drop=True)
+    if 'adj_close' in df_source.columns:  # Change to 'adj_close'
+        df_source = df_source.drop(columns=['adj_close'])
+
+
+    return df_source
+
+
 
 
 # %%
 def minmaxTwoMeasures(df, measureMin, measureMax, column, order=2):
-    
+
+
     df['DateTmp'] = df.index
     x = np.array(df["DateTmp"].values)
     y1 = np.array(df[measureMin].values)
     y2 = np.array(df[measureMax].values)
+
 
     sortId = np.argsort(x)
     x = x[sortId]
     y1 = y1[sortId]
     y2 = y2[sortId]
 
+
     df[column] = 0
+
 
     maxm = signal.argrelextrema(y2, np.greater, order=order)
     minm = signal.argrelextrema(y1, np.less, order=order)
     for elem in maxm[0]:
-        df.iloc[elem, df.columns.get_loc(column)] = 1 
+        df.iloc[elem, df.columns.get_loc(column)] = 1
     for elem in minm[0]:
         df.iloc[elem, df.columns.get_loc(column)] = -1
-    return  df.drop(columns=['DateTmp'])
+    return df.drop(columns=['DateTmp'])
 
 
-def isMin(df,i):
+
+
+def isMin(df, i):
     return df["FlowMinMax"].iat[i] == -1
 
-def isMax(df,i):
+
+
+
+def isMax(df, i):
     return df["FlowMinMax"].iat[i] == 1
 
 
-def distance(x1,y1,x2,y2):  
-     dist = math.sqrt((x2 - x1)**2 + (y2 - y1)**2)  
-     return dist  
 
-def isElliottWave(df,value,i0,i1,i2,i3,i4,i5,ia,ib,ic):
+
+def distance(x1, y1, x2, y2):
+    dist = math.sqrt((x2 - x1)**2 + (y2 - y1)**2)
+    return dist
+
+
+
+
+def isElliottWave(df, value, i0, i1, i2, i3, i4, i5, ia, ib, ic):
     result = None
 
-    if not isMin(df,i0) or not isMin(df,i2) or not isMin(df,i4) or not isMin(df,ia) or not isMin(df,ic):
+
+    # Check if indices are within DataFrame bounds
+    indices = [i0, i1, i2, i3, i4, i5, ia, ib, ic]
+    if any(i >= len(df) for i in indices):  # Check if i is within dataFrame bounds
+        return None
+
+
+    #Check if i1,i2,i3,i4,i5 are integers
+    if not all(isinstance(i, int) for i in [i1, i2, i3, i4, i5]):
+        print("Error: i1, i2, i3, i4, i5 must be integers.")
+        return None
+
+
+    # Get the numeric column index
+    value_col_index = df.columns.get_loc(value)
+
+
+    # Use .iloc[] instead of .iat[] for indexing
+    isi5TheTop = (df.iloc[i5, value_col_index] > df.iloc[i1, value_col_index]) & \
+                  (df.iloc[i5, value_col_index] > df.iloc[i2, value_col_index]) & \
+                  (df.iloc[i5, value_col_index] > df.iloc[i3, value_col_index]) & \
+                  (df.iloc[i5, value_col_index] > df.iloc[i4, value_col_index])
+   
+   
+   
+
+
+
+
+    if not isMin(df, i0) or not isMin(df, i2) or not isMin(
+            df, i4) or not isMin(df, ia) or not isMin(df, ic):
         return result
 
-    if not isMax(df,i1) or not isMax(df,i3) or not isMax(df,i5) or not isMax(df,ib):
+
+    if not isMax(df, i1) or not isMax(df, i3) or not isMax(
+            df, i5) or not isMax(df, ib):
         return result
 
-    isi5TheTop = df[value].iat[i5] > df[value].iat[i1] and df[value].iat[i5] > df[value].iat[i2] and df[value].iat[i5] > df[value].iat[i3] and df[value].iat[i5] > df[value].iat[i4]  
-    if not isi5TheTop:
+
+    # Correct the comparison using bitwise operators
+    isi5TheTop = (df[value].iloc[i5] > df[value].iloc[i1]) & (df[value].iloc[i5] > df[value].iloc[i2]) & (df[value].iloc[i5] > df[value].iloc[i3]) & (df[value].iloc[i5] > df[value].iloc[i4])
+   
+    if not isi5TheTop.any():
         return result
 
-    if not df[value].iat[i1] > df[value].iat[i0]:
+
+    if not df[value].iloc[i1] > df[value].iloc[i0].any():
         return result
 
-    if not df[value].iat[i1] > df[value].iat[i2]:
-        return result
-    
-    if not df[value].iat[i2] > df[value].iat[i0]:
-        return result
-       
-    if not df[value].iat[i3] > df[value].iat[i2]:
+
+    if not df[value].iloc[i1] > df[value].iloc[i2]:
         return result
 
-    w1Len = distance(i0,df[value].iat[i0],i1,df[value].iat[i1])
-    
-    if not df[value].iat[i2] > df[value].iat[i0]:
-        return result
-    
-    if not df[value].iat[i3] > df[value].iat[i4]:
+
+    if not df[value].iloc[i2] > df[value].iloc[i0]:
         return result
 
-    if not df[value].iat[i4] > df[value].iat[i2]:
-        return result
-    w3Len = distance(i2,df[value].iat[i2],i3,df[value].iat[i3])
 
-    if not df[value].iat[i4] > df[value].iat[i1]:
-        return result
-    if not df[value].iat[i5] > df[value].iat[i4]:
+    if not df[value].iloc[i3] > df[value].iloc[i2]:
         return result
 
-    if not df[value].iat[i5] > df[value].iat[i3]:
+
+    w1Len = distance(i0, df[value].iloc[i0], i1, df[value].iloc[i1])
+
+
+    if not df[value].iloc[i2] > df[value].iloc[i0]:
         return result
-    w5Len = distance(i4,df[value].iat[i4],i5,df[value].iat[i5])
+
+
+    if not df[value].iloc[i3] > df[value].iloc[i4]:
+        return result
+
+
+    if not df[value].iloc[i4] > df[value].iloc[i2]:
+        return result
+    w3Len = distance(i2, df[value].iloc[i2], i3, df[value].iloc[i3])
+
+
+    if not df[value].iloc[i4] > df[value].iloc[i1]:
+        return result
+    if not df[value].iloc[i5] > df[value].iloc[i4]:
+        return result
+
+
+    if not df[value].iloc[i5] > df[value].iloc[i3]:
+        return result
+    w5Len = distance(i4, df[value].iloc[i4], i5, df[value].iloc[i5])
+
 
     if (w3Len < w1Len and w3Len < w5Len):
         return result
 
-    result = [i0,i1,i2,i3,i4,i5]
 
-    isi5TheTop = df[value].iat[i5] > df[value].iat[ia]  and df[value].iat[i5] > df[value].iat[ib]  and df[value].iat[i5] > df[value].iat[ic]
+    result = [i0, i1, i2, i3, i4, i5]
+
+
+    # Correct the comparison using bitwise operators
+    isi5TheTop = (df[value].iloc[i5] > df[value].iloc[ia]) & (df[value].iloc[i5] > df[value].iloc[ib]) & (df[value].iloc[i5] > df[value].iloc[ic])
+   
     if not isi5TheTop:
         return result
 
-    if not df[value].iat[i5] > df[value].iat[ia]:
-        return result
-    
-    if not df[value].iat[i5] > df[value].iat[ib]:
+
+    if not df[value].iloc[i5] > df[value].iloc[ia]:
         return result
 
-    if not df[value].iat[ib] > df[value].iat[ia]:
+
+    if not df[value].iloc[i5] > df[value].iloc[ib]:
         return result
 
-    if not df[value].iat[ia] > df[value].iat[ic]:
+
+    if not df[value].iloc[ib] > df[value].iloc[ia]:
         return result
 
-    if not df[value].iat[ib] > df[value].iat[ic]:
+
+    if not df[value].iloc[ia] > df[value].iloc[ic]:
         return result
 
-    result = [i0,i1,i2,i3,i4,i5,ia,ib,ic]
 
-    return result 
+    if not df[value].iloc[ib] > df[value].iloc[ic]:
+        return result
+
+
+    result = [i0, i1, i2, i3, i4, i5, ia, ib, ic]
+
+
+    return result
+
 
 def ElliottWaveDiscovery(df, measure):
 
+
     def minRange(df, start, end):
+
+
         def localFilter(i):
-            return isMin(df,i)
-        return filter(localFilter, list(range(start,end)))
+            return isMin(df, i)
+
+
+        return filter(localFilter, list(range(start, end)))
+
 
     def maxRange(df, start, end):
+
+
         def localFilter(i):
-            return isMax(df,i)
-        return filter(localFilter, list(range(start,end)))
+            return isMax(df, i)
+
+
+        return filter(localFilter, list(range(start, end)))
 
 
     waves = []
-    for i0 in minRange(df,0,len(df)):
-        for i1 in maxRange(df,i0+1,len(df)):
-            for i2 in minRange(df,i1+1,len(df)):
-                for i3 in maxRange(df,i2+1,len(df)):
-                    for i4 in minRange(df,i3+1,len(df)):
-                        for i5 in maxRange(df,i4+1,len(df)):
+    for i0 in minRange(df, 0, len(df)):
+        for i1 in maxRange(df, i0 + 1, len(df)):
+            for i2 in minRange(df, i1 + 1, len(df)):
+                for i3 in maxRange(df, i2 + 1, len(df)):
+                    for i4 in minRange(df, i3 + 1, len(df)):
+                        for i5 in maxRange(df, i4 + 1, len(df)):
 
-                            isi5TheTop = df[measure].iat[i5] > df[measure].iat[i1] and df[measure].iat[i5] > df[measure].iat[i2] and df[measure].iat[i5] > df[measure].iat[i3] and df[measure].iat[i5] > df[measure].iat[i4]  
-                            if isi5TheTop:
 
-                                for ia in minRange(df,i5+1,len(df)):
-                                    for ib in maxRange(df,ia+1,len(df)):
-                                        for ic in minRange(df,ib+1,len(df)):
-                                            wave = isElliottWave(df,measure, i0,i1,i2,i3,i4,i5,ia,ib,ic)
+                            isi5TheTop = (df[measure].iloc[i5] > df[measure].iloc[i1]) & (
+                                df[measure].iloc[i5] > df[measure].iloc[i2]) & (
+                                    df[measure].iloc[i5] > df[measure].iloc[i3]) & (
+                                        df[measure].iloc[i5] > df[measure].iloc[i4])
+                            if isi5TheTop.any():  
+
+
+                                for ia in minRange(df, i5 + 1, len(df)):
+                                    for ib in maxRange(df, ia + 1, len(df)):
+                                        for ic in minRange(df, ib + 1, len(df)):
+                                            wave = isElliottWave(
+                                                df, measure, i0, i1, i2, i3, i4,
+                                                i5, ia, ib, ic)
                                             if wave is None:
                                                 continue
                                             if not wave in waves:
                                                 waves.append(wave)
                                                 print(wave)
 
+
     return waves
+
+
+
+
 # %%
 
-def draw_wave(df,df_waves,w):
+
+def draw_wave(df, df_waves, w):
     fig = plt.figure()
     ax = fig.add_subplot(111)
 
-    ax.plot(df['Close'],       label='Close', color="blue", linestyle="-", alpha=0.5)
-    ax.plot(df_waves['Close'], label='Close', color="black", linestyle="-", alpha = 0.5)
+
+    ax.plot(df['Close'],
+            label='Close',
+            color="blue",
+            linestyle="-",
+            alpha=0.5)
+    ax.plot(df_waves['Close'],
+            label='Close',
+            color="black",
+            linestyle="-",
+            alpha=0.5)
+
 
     ax.plot(df_waves['Close'], 'ko', markevery=None)
 
+
     df_waves["wave"] = None
-    for i in range(0,len(w)):
-            df_waves['wave'].iat[w[i]] = df_waves['Close'].iat[w[i]]
+    for i in range(0, len(w)):
+        df_waves['wave'].iat[w[i]] = df_waves['Close'].iat[w[i]]
+
 
     df_filtered_waves = df_waves.loc[pd.notnull(df_waves.wave)]
     ax.plot(df_filtered_waves['wave'], color="red", linewidth=3.0)
-    plt.show()   
+    plt.show()
+
+
+
+
 # %%
+
 
 # select the waves the best fit the chart
 def filterWaveSet(waves, min_len=6, max_len=6, extremes=True):
+
 
     result = []
     for w in waves:
         l = len(w)
         if min_len <= l and l <= max_len:
             result.append(w)
-    
+
+
     if not extremes:
         return result
 
-    max = 0 
+
+    max = 0
     for w in result:
-        if w[len(w)-1] >= max:
-            max = w[len(w)-1]
-        
+        if w[len(w) - 1] >= max:
+            max = w[len(w) - 1]
+
+
     result2 = []
     for w in result:
-        if w[len(w)-1] == max:
+        if w[len(w) - 1] == max:
             result2.append(w)
+
 
     min = max
     for w in result2:
         if w[0] <= min:
-            min = w[0]    
+            min = w[0]
+
 
     result = []
     for w in result2:
         if w[0] == min:
             result.append(w)
 
+
     return result
+
+
+
+
 # %%
 import math
 
-def line(wa,wb, x):
+
+
+
+def line(wa, wb, x):
     x1 = wa[0]
     y1 = wa[1]
     x2 = wb[0]
     y2 = wb[1]
-    y = ((y2-y1)/(x2-x1))*(x-x1) + y1
+    y = ((y2 - y1) / (x2 - x1)) * (x - x1) + y1
     return y
+
+
 
 
 def elliottWaveLinearRegressionError(df_waves, w, value):
     diffquad = 0
-    for i in range(1,len(w)):
-        wa = [ w[i-1], df_waves[value].iat[w[i-1]] ]
-        wb = [ w[i  ], df_waves[value].iat[w[i  ]] ]
-        
-        for xindex in range(wa[0],wb[0]):
+    for i in range(1, len(w)):
+        wa = [w[i - 1], df_waves[value].iat[w[i - 1]]]
+        wb = [w[i], df_waves[value].iat[w[i]]]
+
+
+        for xindex in range(wa[0], wb[0]):
             yindex = df_waves[value].iat[xindex]
-            yline = line(wa,wb, xindex)
+            yline = line(wa, wb, xindex)
 
-            diffquad += (yindex-yline) ** 2
 
-    return math.sqrt(diffquad)/(w[len(w)-1]-w[0])
+            diffquad += (yindex - yline)**2
 
-def findBestFitWave(df,value,waves):
+
+    return math.sqrt(diffquad) / (w[len(w) - 1] - w[0])
+
+
+
+
+def findBestFitWave(df, value, waves):
+
 
     avg = np.Inf
-    df_waves = df[[value,"FlowMinMax"]]
+    df_waves = df[[value, "FlowMinMax"]]
     result = []
     for w in waves:
         tmp = elliottWaveLinearRegressionError(df_waves, w, value)
 
+
         if tmp < avg:
-            print(w,tmp)
+            print(w, tmp)
             avg = tmp
             result = w
     return result
+
+
+
+
 # %%
 
+
 def buildWaveChainSet(waves, startwith=9):
+
 
     def addList(list, wavelist):
         k = 0
@@ -328,42 +498,61 @@ def buildWaveChainSet(waves, startwith=9):
         if len(w1) == 9:
             for w2 in waves:
                 if (len(w2) <= len(w1)):
-                    if w1[len(w1)-1] == w2[0]:
+                    if w1[len(w1) - 1] == w2[0]:
                         wavelist.append(w2)
                         addList(list, wavelist.copy())
                         wavelist.pop(-1)
         else:
             addList(list, wavelist)
-    return list           
+    return list
+
+
+
+
 # %%
+
 
 # %%
 # -------------------------------------------------
 #  given a timeline, we generate all the possible waves
 # -------------------------------------------------
 
+
 if date is None:
     date = dt.datetime.now().strftime("%Y-%m-%d")
-df_source = download(symbol, date, 365*5)
+df_source = download(symbol, date, 365 * 5)
 
-df_source["Date"] = pd.to_datetime(df_source["Date"], infer_datetime_format=True)
+
+df_source["Date"] = pd.to_datetime(df_source["Date"],
+                                  infer_datetime_format=True)
 df_source.set_index("Date")
+
+
+
+
 # %%
-def ElliottWaveFindPattern(df_source, measure, granularity, dateStart, dateEnd, today, extremes=True):
+def ElliottWaveFindPattern(df_source, measure, granularity, dateStart,
+                           dateEnd, today, extremes=True):
+
 
     mask = (dateStart <= df_source.Date) & (df_source.Date <= dateEnd)
     df = df_source.loc[mask]
     df.set_index("Date")
 
-    FlowMinMax = minmaxTwoMeasures(df,"Close","Close","FlowMinMax",granularity)
+
+    FlowMinMax = minmaxTwoMeasures(df, "Close", "Close", "FlowMinMax",
+                                   granularity)
+
 
     df = FlowMinMax
     df_samples = df.loc[df['FlowMinMax'] != 0]
 
+
     draw_wave(df, df_samples, [])
 
+
     print("start ", len(FlowMinMax))
-    waves =  ElliottWaveDiscovery(df_samples[[measure,"FlowMinMax"]], measure)
+    waves = ElliottWaveDiscovery(df_samples[["Close", "FlowMinMax"]], "Close")
     print("waves")
     print(waves)
     filtered_waves = filterWaveSet(waves, 5, 9, extremes=extremes)
@@ -375,11 +564,33 @@ def ElliottWaveFindPattern(df_source, measure, granularity, dateStart, dateEnd, 
             waves_for_len[len(w)] = []
         waves_for_len[len(w)].append(w)
 
-    for k, v in waves_for_len.items():
-        result = findBestFitWave(df_samples, measure, v)  # Note: v here is the value
+
+    for k, v in waves_for_len.items(
+    ):  # Use .items() to get both keys and values
+        result = findBestFitWave(df_samples, "Close",
+                                 v)  # Note: v here is the value
         print("best fit")
         print(result)
         draw_wave(df, df_samples, result)
 
 
-ElliottWaveFindPattern(df_source, "Close", 7, "2019-03-01", today, today, extremes=False)
+        # Add Buy/Sell/Hold Logic (Simplified Example)
+        if len(result) >= 9:  # Assuming 9 points represent a complete pattern
+            # Check the trend of the last wave (wave 5)
+            if df_samples["Close"].iat[result[
+                    4]] < df_samples["Close"].iat[result[5]]:  # Uptrend
+                print("Signal: Buy")
+            elif df_samples["Close"].iat[result[
+                    4]] > df_samples["Close"].iat[result[5]]:  # Downtrend
+                print("Signal: Sell")
+            else:
+                print("Signal: Hold")
+        else:
+            print("Signal: Hold (Incomplete Pattern)")
+
+
+
+
+ElliottWaveFindPattern(df_source, "Close", 7, "2019-03-01", today, today,
+                           extremes=False)
+
